76ee1f4340933431221834250cc3daac63689d54
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-11-30 08:41:11 +0530

    Adding code to check if user entered valid first Name

--- src/com/bridgelabz/user_registration/UserRegistrationForm.java
+++ src/com/bridgelabz/user_registration/UserRegistrationForm.java
@@ -1,7 +1,24 @@
 package com.bridgelabz.user_registration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistrationForm {
 	public static void main(String[] args) {
+		//Master
 		System.out.println("Welcome to User Registration program");
+		//UC1:First Name
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Please enter your First Name: ");
+		String firstName=sc.next();
+		boolean res=Pattern.matches("^[A-Z][a-z]{2,}$", firstName);
+		if(res) {
+			System.out.println("You have entered valid First Name");
+		}else {
+			System.out.println("Sorry! you have entered invalid First Name");
+		}
+		
+		
+
 	}
 }
