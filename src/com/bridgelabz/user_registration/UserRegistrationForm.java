c1add1b35359d8ade5bfe155c8f26637c6be641d
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-11-30 13:33:51 +0530

    Refactor the code with the user input mobile number

--- src/com/bridgelabz/user_registration/UserRegistrationForm.java
+++ src/com/bridgelabz/user_registration/UserRegistrationForm.java
@@ -10,7 +10,7 @@ public class UserRegistrationForm {
 		// UC1:First Name
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Please enter your First Name: ");
-		String firstName = sc.next();
+		String firstName = sc.nextLine();
 		boolean res = Pattern.matches("^[A-Z][a-z]{2,}$", firstName);
 		if (res) {
 			System.out.println("Awesome! You have entered valid First Name");
@@ -20,7 +20,7 @@ public class UserRegistrationForm {
 
 		// UC2:Last Name
 		System.out.println("Please enter your Last Name: ");
-		String lastName = sc.next();
+		String lastName = sc.nextLine();
 		boolean lastNameCheck = Pattern.matches("^[A-Z][a-z]{2,}$", lastName);
 		if (lastNameCheck) {
 			System.out.println("Fantastic! You have entered valid Last Name");
@@ -30,14 +30,27 @@ public class UserRegistrationForm {
 		
 		//UC3: Email
 		System.out.println("Please enter your Email Address: ");
-		String email = sc.next();
+		String email = sc.nextLine();
 		String emailPattern="^[a-z0-9]{3,}[+.-]?[a-z0-9]{0,}[@]{1,}[a-z0-9]{1,}[.]{1,}([a-z]{0,}[.]{0,})[a-z]{2,}$";
 		boolean emailCheck = Pattern.matches(emailPattern, email);
 		if (emailCheck) {
-			System.out.println("Fantastic! You have entered valid Email Address");
+			System.out.println("Wonderfull! You have entered valid Email Address");
 		} else {
 			System.out.println("Sorry! you have entered invalid Email Address");
 		}
+		
+		//UC4: Mobile Number
+		System.out.println("Please enter your Mobile Number: ");
+		String mobileNumber = sc.nextLine();
+		String mobileNumberPattern="^[+91]{3,}[ ][0-9]{10}$";
+		boolean mnCheck = Pattern.matches(mobileNumberPattern, mobileNumber);
+		if (mnCheck) {
+			System.out.println("Flabbergasted! You have entered valid Mobile Number");
+		} else {
+			System.out.println("Sorry! you have entered invalid Mobile Number");
+		}
 		sc.close();
+		
+		
 	}
 }
