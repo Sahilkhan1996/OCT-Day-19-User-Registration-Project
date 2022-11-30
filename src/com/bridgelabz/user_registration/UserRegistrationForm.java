8292247ebeb7a06293cf8816c28e968293da6af1
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-11-30 08:15:13 +0530

    Initiating User Registration program

---
+++ src/com/bridgelabz/user_registration/UserRegistrationForm.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.user_registration;
+
+public class UserRegistrationForm {
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration program");
+	}
+}
